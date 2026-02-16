c0f2fbc61237056a766f0592da42ce0f60c9da93
Karan C <kc84273@gmail.com>
2026-02-16 16:55:01 +0530

    tiny changes, fully done (for now). will add better code as I learn more things

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,8 +1,13 @@
 #import tkinter 
+#add GUI to this whenever free
 
 decision = input("Hi, What would you like to use? (A. Calculator, B. Word Analysis Tool \nor Type 'e' to Exit): ")
 
 def calc():
+    global input1, input2, operation
+    input1 = int(input("Enter your 1st number: "))
+    operation = input("Enter your operator (+, -, *, /): ")
+    input2 = int(input("Enter your 2nd number: "))
     if operation == "+":
         return input1+input2
         #addition = input1 + input2
@@ -34,14 +39,14 @@ def palindrome():
         if i.isalnum():
             word_list.append(i)
     edited_word = "".join(word_list)
-    print(edited_word)
+    #print(edited_word) for debugging
     def pal(edited_word):
         if edited_word == "":
             return edited_word
         else:
             return pal(edited_word[1:]) + edited_word[0]
     new_word = pal(edited_word)
-    print(new_word)
+    #print(new_word) for debugging
     if edited_word.lower() == new_word.lower():
         return f"\"{word}\" is a palindrome"
     else:
@@ -59,17 +64,13 @@ def reverse():
 
 
 
-
 if decision == "A" or decision == "a":
-    input1 = int(input("Enter your 1st number: "))
-    operation = input("Enter your operator (+, -, *, /): ")
-    input2 = int(input("Enter your 2nd number: "))
     print(f"{input1} {operation} {input2} = {calc()}")
 
 elif decision == "B" or decision == "b":
     input3 = input("What tool would you like to use?\nA. Character Counter\nB. " \
     "Palindrome Checker\nC. Reverse a Word: ")
-    if input3 == "A" or input3 == "a":
+    if input3 == "A" or input3 == "a": 
         char_counter()
     elif input3 == "B" or input3 == "b":
         print(palindrome())
@@ -83,3 +84,14 @@ elif decision == "e":
 
 else:
     print("Please input a valid option")
+
+"""
+2 things I'm not happy with:
+a) int/float user input in calculator - I want 3*4 to be 12 and not 12.0, but if I do int(input)
+then I cannot input a float, the calculator will use only whole numbers ?!?!
+b) I want the user input to repeat, once a task is complete, the main question "What would
+ you like to use? (A. Calculator...." should be asked again instead of pressing the run button 
+ repeatedly. to close the program, the user can input "e". tried this using while loop (even searched
+ it up) but not working
+
+"""
