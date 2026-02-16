376addcd8c6e8593478973d70c07cee4035db7e5
Karan C <kc84273@gmail.com>
2026-02-16 12:06:50 +0530

    palindrome func implemented

--- simple-calculator.py
+++ simple-calculator.py
@@ -27,6 +27,18 @@ def char_counter():
         count+=1
     print(f"There are {count} number of characters in the phrase \"{word}\"")
 
+def palindrome():
+    word = input("Enter your word: ")
+    def pal(word):
+        if word == "":
+            return word
+        else:
+            return pal(word[1:]) + word[0]
+    new_word = pal(word)
+    if word == new_word:
+        return f"{word} is a palindrome"
+    else:
+        return f"{word} is not a palindrome"
 
 
 
@@ -38,14 +50,21 @@ if decision == "A" or decision == "a":
     operation = input("Enter your operator (+, -, *, /): ")
     input2 = int(input("Enter your 2nd number: "))
     print(f"{input1} {operation} {input2} = {calc()}")
-if decision == "B" or decision == "b":
+
+elif decision == "B" or decision == "b":
     input3 = input("What tool would you like to use?\nA. Character Counter\nB. " \
     "Palindrome Checker\nC. Reverse a Word: ")
     if input3 == "A" or input3 == "a":
         char_counter()
-    elif input3 == "B":
-        pass
+    elif input3 == "B" or input3 == "b":
+        print(palindrome())
     elif input3 == "C":
         pass
     else:
-        print("Please enter a valid input")
+        print("Please enter a valid option")
+
+elif decision == "e":
+    print("See you soon!")
+
+else:
+    print("Please input a valid option")
