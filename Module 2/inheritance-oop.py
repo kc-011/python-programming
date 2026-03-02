c41dd44da8fbbe04a0f19378bd2859694489d642
Karan C <kc84273@gmail.com>
2026-03-02 11:21:19 +0530

    basic inheritance

--- Module 2/inheritance-oop.py
+++ Module 2/inheritance-oop.py
@@ -1,23 +1,26 @@
+#Base/Parent/Super class --> Derived/Child/Sub class
+
 class Human:
-    def __init__(self):
+    def __init__(self,num_heart):
         self.num_eyes = 2
         self.num_nose = 1
+        self.num_heart = num_heart
     def eat(self):
         print("I can eat")
     def work(self):
         print("I can work")
 
 class Male(Human):
-    def __init__(self,name):
-        super().__init__()
+    def __init__(self,name, num_heart):
+        super().__init__(num_heart)
         self.name = name
-    def flirt(self):
-        print("I can flirt")
+    def jump(self):
+        print("I can jump")
     def work(self):
         super().work()
         print("I can code")
 
-male_1 = Male("Karan")
+male_1 = Male("Karan",1)
 male_1.eat()
 male_1.work()
-print(male_1.num_eyes)
+print(male_1.num_heart)
